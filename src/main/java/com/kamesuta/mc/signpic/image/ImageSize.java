774e62e5a36bea3371555e08852b0374baf7206b
Kamesuta <kamesuta@gmail.com>
2016-08-19 16:16:10 +0900

    Sizes

--- src/main/java/com/kamesuta/mc/signpic/image/ImageSize.java
+++ src/main/java/com/kamesuta/mc/signpic/image/ImageSize.java
@@ -9,80 +9,33 @@ public class ImageSize {
 		this.height = height;
 	}
 
-	//public static ImageSize from
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(this.height);
+		result = prime * result + Float.floatToIntBits(this.width);
+		return result;
+	}
 
-	public static enum Size {
-		RAW {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				return new ImageSize(w, h);
-			}
-		},
-		MAX {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				return new ImageSize(maxw, maxh);
-			}
-		},
-		WIDTH {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				return new ImageSize(w, maxh*w/maxw);
-			}
-		},
-		HEIGHT {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				return new ImageSize(maxw*h/maxh, h);
-			}
-		},
-		OUTER {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				return new ImageSize((maxw/w>maxh/h)?w:maxw*h/maxh, (maxw/w>maxh/h)?maxh*w/maxw:h);
-			}
-		},
-		INNER {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				return new ImageSize((maxw/w<maxh/h)?w:maxw*h/maxh, (maxw/w<maxh/h)?maxh*w/maxw:h);
-			}
-		},
-		WIDTH_LIMIT {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				if (w<maxw)
-					return new ImageSize(w, h);
-				else
-					return new ImageSize(maxw, maxw*h/w);
-			}
-		},
-		HEIGHT_LIMIT {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				if (h<maxh)
-					return new ImageSize(w, h);
-				else
-					return new ImageSize(maxh, maxh*w/h);
-			}
-		},
-		LIMIT {
-			@Override
-			public ImageSize size(final float w, final float h, final float maxw, final float maxh) {
-				if (w>h)
-					if (w<maxw)
-						return new ImageSize(w, h);
-					else
-						return new ImageSize(maxw, maxw*h/w);
-				else
-					if (h<maxh)
-						return new ImageSize(w, h);
-					else
-						return new ImageSize(maxh, maxh*w/h);
-			}
-		},
-		;
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof ImageSize))
+			return false;
+		final ImageSize other = (ImageSize) obj;
+		if (Float.floatToIntBits(this.height) != Float.floatToIntBits(other.height))
+			return false;
+		if (Float.floatToIntBits(this.width) != Float.floatToIntBits(other.width))
+			return false;
+		return true;
+	}
 
-		public abstract ImageSize size(float w, float h, float maxw, float maxh);
+	@Override
+	public String toString() {
+		return String.format("ImageSize[width:%s, height:%s]", this.width, this.height);
 	}
 }
