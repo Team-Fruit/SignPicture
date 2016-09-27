495b5aab77a8de6ca7e655701f2005bdf631c26d
Kamesuta <kamesuta@gmail.com>
2016-09-27 22:15:35 +0900

    #

---
+++ src/main/java/com/kamesuta/mc/signpic/Locations.java
@@ -0,0 +1,46 @@
+package com.kamesuta.mc.signpic;
+
+import java.io.File;
+
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class Locations {
+	public File mcDir;
+	public File signpicDir;
+	public File cacheDir;
+	public File metaDir;
+	public File configDir;
+	public File configFile;
+	public File modDir;
+	public File modFile;
+
+	public Locations(final FMLPreInitializationEvent event, final File mcdir) {
+		this.signpicDir = new File(mcdir, "signpic");
+		securementDirectory(this.signpicDir);
+		this.cacheDir = new File(this.signpicDir, "cache");
+		securementDirectory(this.cacheDir);
+		this.metaDir = new File(this.signpicDir, "meta");
+		securementDirectory(this.metaDir);
+
+		this.configDir = event.getModConfigurationDirectory();
+		this.configFile = event.getSuggestedConfigurationFile();
+		this.modDir = new File(mcdir, "mods");
+		this.modFile = event.getSourceFile();
+	}
+
+	private boolean securementDirectory(final File cachedir) {
+		if (cachedir.exists() && !cachedir.isDirectory()) {
+			File to;
+			int i = 2;
+			do {
+				to = new File(cachedir.getParent(), cachedir.getName()+i);
+				i++;
+			} while (to.exists());
+			cachedir.renameTo(to);
+			Reference.logger.warn("non-directory conflicting file exists. renamed to " + to.getName());
+			return true;
+		}
+		cachedir.mkdir();
+		return false;
+	}
+}
