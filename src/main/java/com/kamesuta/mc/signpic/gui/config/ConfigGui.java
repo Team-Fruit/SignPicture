eb0538f3135bdc85f0881339593651cb5137c8eb
Kamesuta <kamesuta@gmail.com>
2016-12-04 00:02:52 +0900

    #

--- src/main/java/com/kamesuta/mc/signpic/gui/config/ConfigGui.java
+++ src/main/java/com/kamesuta/mc/signpic/gui/config/ConfigGui.java
@@ -1,4 +1,5 @@
 package com.kamesuta.mc.signpic.gui.config;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,40 +12,24 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.ConfigElement;
 
-public class ConfigGui extends GuiConfig
-{
+public class ConfigGui extends GuiConfig {
 
-	public ConfigGui( final GuiScreen parent )
-	{
-		super( parent, getConfigElements(), Reference.MODID, false, false, GuiConfig.getAbridgedConfigPath( Config.instance.getFilePath() ) );
+	public ConfigGui(final GuiScreen parent) {
+		super(parent, getConfigElements(), Reference.MODID, false, false, GuiConfig.getAbridgedConfigPath(Config.instance.getFilePath()));
 	}
 
 	@SuppressWarnings("rawtypes")
-	private static List<IConfigElement> getConfigElements()
-	{
+	private static List<IConfigElement> getConfigElements() {
 		final List<IConfigElement> list = new ArrayList<IConfigElement>();
 
-		for( final String cat : Config.instance.getCategoryNames() )
-		{
-			if( cat.equals( "versionchecker" ) )
-			{
+		for (final String cat : Config.instance.getCategoryNames()) {
+			final ConfigCategory cc = Config.instance.getCategory(cat);
+
+			if (cc.isChild())
 				continue;
-			}
 
-			if( cat.equals( "settings" ) )
-			{
-				continue;
-			}
-
-			final ConfigCategory cc = Config.instance.getCategory( cat );
-
-			if( cc.isChild() )
-			{
-				continue;
-			}
-
-			final ConfigElement ce = new ConfigElement<String>( cc );
-			list.add( ce );
+			final ConfigElement ce = new ConfigElement<String>(cc);
+			list.add(ce);
 		}
 
 		return list;
--- src/main/java/com/kamesuta/mc/signpic/gui/config/ConfigGuiFactory.java
+++ src/main/java/com/kamesuta/mc/signpic/gui/config/ConfigGuiFactory.java
@@ -1,35 +1,30 @@
 package com.kamesuta.mc.signpic.gui.config;
+
 import java.util.Set;
 
 import cpw.mods.fml.client.IModGuiFactory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 
-
-public class ConfigGuiFactory implements IModGuiFactory
-{
+public class ConfigGuiFactory implements IModGuiFactory {
 
 	@Override
-	public void initialize( final Minecraft minecraftInstance )
-	{
+	public void initialize(final Minecraft minecraftInstance) {
 
 	}
 
 	@Override
-	public Class<? extends GuiScreen> mainConfigGuiClass()
-	{
+	public Class<? extends GuiScreen> mainConfigGuiClass() {
 		return ConfigGui.class;
 	}
 
 	@Override
-	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
-	{
+	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
 		return null;
 	}
 
 	@Override
-	public RuntimeOptionGuiHandler getHandlerFor( final RuntimeOptionCategoryElement element )
-	{
+	public RuntimeOptionGuiHandler getHandlerFor(final RuntimeOptionCategoryElement element) {
 		return null;
 	}
 }
