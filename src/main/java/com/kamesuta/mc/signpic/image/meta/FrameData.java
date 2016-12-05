b3cc5032a8d2005a6d631461d37213f3ec7310e7
Kamesuta <kamesuta@gmail.com>
2016-12-05 23:00:49 +0900

    framedata

---
+++ src/main/java/com/kamesuta/mc/signpic/image/meta/FrameData.java
@@ -0,0 +1,51 @@
+package com.kamesuta.mc.signpic.image.meta;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.kamesuta.mc.bnnwidget.motion.Easings;
+
+public class FrameData {
+	public final Easings easing;
+	public final RSNeed redstone;
+
+	public FrameData(final Easings easing2, final RSNeed redstone) {
+		this.easing = easing2;
+		this.redstone = redstone;
+	}
+
+	public String compose() {
+		final StringBuilder stb = new StringBuilder();
+		if (this.easing!=Easings.easeLinear)
+			stb.append("k").append(this.easing.id);
+		if (this.redstone!=RSNeed.IGNORE)
+			stb.append("t").append(this.redstone.id);
+		return stb.toString();
+	}
+
+	public static enum RSNeed {
+		IGNORE(0), RS_ON(1), RS_OFF(2),
+		;
+
+		public final int id;
+
+		private RSNeed(final int id) {
+			this.id = id;
+		}
+
+		private static final ImmutableMap<Integer, RSNeed> rsIds;
+
+		public static RSNeed fromId(final int id) {
+			RSNeed rs = rsIds.get(id);
+			if (rs==null)
+				rs = IGNORE;
+			return rs;
+		}
+
+		static {
+			final Builder<Integer, RSNeed> builder = ImmutableMap.builder();
+			for (final RSNeed easing : RSNeed.values())
+				builder.put(easing.id, easing);
+			rsIds = builder.build();
+		}
+	}
+}
---
+++ src/main/java/com/kamesuta/mc/signpic/image/meta/ImageFrame.java
@@ -0,0 +1,47 @@
+package com.kamesuta.mc.signpic.image.meta;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import com.kamesuta.mc.bnnwidget.motion.Easings;
+import com.kamesuta.mc.signpic.image.meta.FrameData.RSNeed;
+
+public class ImageFrame extends ImageMeta.MetaParser {
+
+	public Easings easing = Easings.easeLinear;
+	public RSNeed redstone = RSNeed.RS_OFF;
+
+	public FrameData get() {
+		return new FrameData(this.easing, this.redstone);
+	}
+
+	@Override
+	public ImageFrame reset() {
+		this.easing = Easings.easeLinear;
+		this.redstone = RSNeed.RS_OFF;
+		return this;
+	}
+
+	@Override
+	public boolean parse(final String src, final String key, final String value) {
+		if (StringUtils.equals(key, "k"))
+			this.easing = Easings.fromId(NumberUtils.toInt(value));
+		else if (StringUtils.equals(key, "t"))
+			this.redstone = RSNeed.fromId(NumberUtils.toInt(value));
+		else
+			return false;
+		return true;
+	}
+
+	@Deprecated
+	@Override
+	public String compose() {
+		return get().compose();
+	}
+
+	@Deprecated
+	@Override
+	public String toString() {
+		return compose();
+	}
+}
