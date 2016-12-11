d19219f38077e49eeb71443bb975358528bab731
Kamesuta <kamesuta@gmail.com>
2016-12-12 01:59:09 +0900

    #

--- src/main/java/com/kamesuta/mc/signpic/image/meta/ImageMeta.java
+++ src/main/java/com/kamesuta/mc/signpic/image/meta/ImageMeta.java
@@ -1,6 +1,7 @@
 package com.kamesuta.mc.signpic.image.meta;
 
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,6 +11,7 @@ import org.apache.commons.lang3.Validate;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.kamesuta.mc.bnnwidget.motion.Easings;
 import com.kamesuta.mc.signpic.Reference;
 import com.kamesuta.mc.signpic.image.meta.ImageTextureMap.ImageTextureMapBoolean;
@@ -26,18 +28,25 @@ public class ImageMeta {
 
 	private boolean hasInvalidMeta;
 
-	public final MovieMeta<AnimationData, AnimationData, AnimationData> animations = new MovieMeta<AnimationData, AnimationData, AnimationData>(new MovieBuilder<AnimationData, AnimationData>() {
+	private Set<MovieMeta<?, ?, ?>> metas = Sets.newHashSet();
+
+	private <E extends MovieMeta<?, ?, ?>> E add(final E e) {
+		this.metas.add(e);
+		return e;
+	}
+
+	public final MovieMeta<AnimationData, AnimationData, AnimationData> animations = add(new MovieMeta<AnimationData, AnimationData, AnimationData>(new MovieBuilder<AnimationData, AnimationData>() {
 		@Override
 		public MetaMovie<AnimationData, AnimationData> builder() {
 			return new ImageAnimation();
 		}
-	});
-	public final MovieMeta<SizeData, SizeData, SizeData> sizes = new MovieMeta<SizeData, SizeData, SizeData>(new MovieBuilder<SizeData, SizeData>() {
+	}));
+	public final MovieMeta<SizeData, SizeData, SizeData> sizes = add(new MovieMeta<SizeData, SizeData, SizeData>(new MovieBuilder<SizeData, SizeData>() {
 		@Override
 		public ImageSize builder() {
 			return new ImageSize();
 		}
-	});
+	}));
 
 	private static class OffsetDataBuilder implements MovieBuilder<OffsetData, OffsetData> {
 		private String neg;
@@ -54,15 +63,15 @@ public class ImageMeta {
 		}
 	}
 
-	public final MovieMeta<OffsetData, OffsetData, OffsetData> xoffsets = new MovieMeta<OffsetData, OffsetData, OffsetData>(new OffsetDataBuilder("L", "R"));
-	public final MovieMeta<OffsetData, OffsetData, OffsetData> yoffsets = new MovieMeta<OffsetData, OffsetData, OffsetData>(new OffsetDataBuilder("D", "U"));
-	public final MovieMeta<OffsetData, OffsetData, OffsetData> zoffsets = new MovieMeta<OffsetData, OffsetData, OffsetData>(new OffsetDataBuilder("B", "F"));
-	public final MovieMeta<KeyRotation, RotationData, DiffRotation> rotations = new MovieMeta<KeyRotation, RotationData, DiffRotation>(new MovieBuilder<DiffRotation, KeyRotation>() {
+	public final MovieMeta<OffsetData, OffsetData, OffsetData> xoffsets = add(new MovieMeta<OffsetData, OffsetData, OffsetData>(new OffsetDataBuilder("L", "R")));
+	public final MovieMeta<OffsetData, OffsetData, OffsetData> yoffsets = add(new MovieMeta<OffsetData, OffsetData, OffsetData>(new OffsetDataBuilder("D", "U")));
+	public final MovieMeta<OffsetData, OffsetData, OffsetData> zoffsets = add(new MovieMeta<OffsetData, OffsetData, OffsetData>(new OffsetDataBuilder("B", "F")));
+	public final MovieMeta<KeyRotation, RotationData, DiffRotation> rotations = add(new MovieMeta<KeyRotation, RotationData, DiffRotation>(new MovieBuilder<DiffRotation, KeyRotation>() {
 		@Override
 		public ImageRotation builder() {
 			return new ImageRotation();
 		}
-	});
+	}));
 
 	private static class TexDataBuilder implements MovieBuilder<TextureMapData, TextureMapData> {
 		private DataType type;
@@ -77,13 +86,13 @@ public class ImageMeta {
 		}
 	}
 
-	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> u = new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.U));
-	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> v = new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.V));
-	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> w = new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.W));
-	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> h = new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.H));
-	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> c = new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.C));
-	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> s = new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.S));
-	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> o = new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.O));
+	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> u = add(new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.U)));
+	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> v = add(new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.V)));
+	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> w = add(new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.W)));
+	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> h = add(new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.H)));
+	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> c = add(new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.C)));
+	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> s = add(new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.S)));
+	public final MovieMeta<TextureMapData, TextureMapData, TextureMapData> o = add(new MovieMeta<TextureMapData, TextureMapData, TextureMapData>(new TexDataBuilder(DataType.O)));
 
 	private static class TexDataBooleanBuilder implements MovieBuilder<TextureMapDataBoolean, TextureMapDataBoolean> {
 		private DataTypeBoolean type;
@@ -98,8 +107,8 @@ public class ImageMeta {
 		}
 	}
 
-	public final MovieMeta<TextureMapDataBoolean, TextureMapDataBoolean, TextureMapDataBoolean> r = new MovieMeta<TextureMapData.TextureMapDataBoolean, TextureMapData.TextureMapDataBoolean, TextureMapData.TextureMapDataBoolean>(new TexDataBooleanBuilder(DataTypeBoolean.R));
-	public final MovieMeta<TextureMapDataBoolean, TextureMapDataBoolean, TextureMapDataBoolean> m = new MovieMeta<TextureMapData.TextureMapDataBoolean, TextureMapData.TextureMapDataBoolean, TextureMapData.TextureMapDataBoolean>(new TexDataBooleanBuilder(DataTypeBoolean.M));
+	public final MovieMeta<TextureMapDataBoolean, TextureMapDataBoolean, TextureMapDataBoolean> r = add(new MovieMeta<TextureMapData.TextureMapDataBoolean, TextureMapData.TextureMapDataBoolean, TextureMapData.TextureMapDataBoolean>(new TexDataBooleanBuilder(DataTypeBoolean.R)));
+	public final MovieMeta<TextureMapDataBoolean, TextureMapDataBoolean, TextureMapDataBoolean> m = add(new MovieMeta<TextureMapData.TextureMapDataBoolean, TextureMapData.TextureMapDataBoolean, TextureMapData.TextureMapDataBoolean>(new TexDataBooleanBuilder(DataTypeBoolean.M)));
 
 	public ImageMeta(final String src) {
 		Validate.notNull(src);
@@ -138,22 +147,10 @@ public class ImageMeta {
 					final String key = mp.group(1);
 					final String value = 2<=gcount ? mp.group(2) : "";
 					if (!StringUtils.isEmpty(key)||!StringUtils.isEmpty(value)) {
-						final boolean ia = this.animations.parse(src, key, value);
-						final boolean ib = this.sizes.parse(src, key, value);
-						final boolean ic1 = this.xoffsets.parse(src, key, value);
-						final boolean ic2 = this.yoffsets.parse(src, key, value);
-						final boolean ic3 = this.zoffsets.parse(src, key, value);
-						final boolean id = this.rotations.parse(src, key, value);
-						final boolean ie1 = this.u.parse(src, key, value);
-						final boolean ie2 = this.v.parse(src, key, value);
-						final boolean ie3 = this.w.parse(src, key, value);
-						final boolean ie4 = this.h.parse(src, key, value);
-						final boolean ie5 = this.c.parse(src, key, value);
-						final boolean ie6 = this.s.parse(src, key, value);
-						final boolean ie7 = this.o.parse(src, key, value);
-						final boolean ie8 = this.r.parse(src, key, value);
-						final boolean ie9 = this.m.parse(src, key, value);
-						bb = (ia||ib||ic1||ic2||ic3||id||ie1||ie2||ie3||ie4||ie5||ie6||ie7||ie8||ie9)&&bb;
+						boolean b = false;
+						for (final MovieMeta<?, ?, ?> m : this.metas)
+							b = m.parse(src, key, value)||b;
+						bb = b&&bb;
 					}
 				}
 			}
@@ -163,21 +160,9 @@ public class ImageMeta {
 				final AnimationData anim = this.animations.getDiff();
 				easing = anim.easing;
 			}
-			this.animations.next(time, easing);
-			this.sizes.next(time, easing);
-			this.xoffsets.next(time, easing);
-			this.yoffsets.next(time, easing);
-			this.zoffsets.next(time, easing);
-			this.rotations.next(time, easing);
-			this.u.next(time, easing);
-			this.v.next(time, easing);
-			this.w.next(time, easing);
-			this.h.next(time, easing);
-			this.c.next(time, easing);
-			this.s.next(time, easing);
-			this.o.next(time, easing);
-			this.r.next(time, easing);
-			this.m.next(time, easing);
+
+			for (final MovieMeta<?, ?, ?> m : this.metas)
+				m.next(time, easing);
 		}
 
 		this.hasInvalidMeta = this.hasInvalidMeta||!bb;
