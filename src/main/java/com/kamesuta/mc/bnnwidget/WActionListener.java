9b394d58573798889ce4f60b5492a13968f823ca
sjcl <fruit@bebehp.com>
2017-02-09 15:58:31 +0900

    Submodule

--- src/main/java/com/kamesuta/mc/bnnwidget/WActionListener.java
+++
@@ -1,5 +0,0 @@
-package com.kamesuta.mc.bnnwidget;
-
-public interface WActionListener {
-	public void actionPerformed(String command, Object... params);
-}
--- src/main/java/com/kamesuta/mc/bnnwidget/WPanel.java
+++
@@ -1,9 +0,0 @@
-package com.kamesuta.mc.bnnwidget;
-
-import com.kamesuta.mc.bnnwidget.position.R;
-
-public class WPanel extends WTypedPanel<WCommon> {
-	public WPanel(final R position) {
-		super(position);
-	}
-}
