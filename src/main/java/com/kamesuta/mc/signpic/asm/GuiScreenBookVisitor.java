3565bdd234d7f234b8bd9be47c6fee85393d5e8b
Kamesuta <kamesuta@gmail.com>
2017-02-18 22:54:02 +0900

    book

---
+++ src/main/java/com/kamesuta/mc/signpic/asm/GuiScreenBookVisitor.java
@@ -0,0 +1,64 @@
+package com.kamesuta.mc.signpic.asm;
+
+import javax.annotation.Nullable;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import com.kamesuta.mc.signpic.asm.lib.DescHelper;
+import com.kamesuta.mc.signpic.asm.lib.MethodMatcher;
+
+public class GuiScreenBookVisitor extends ClassVisitor {
+	private static class HookMethodVisitor extends MethodVisitor {
+		private final MethodMatcher matcher;
+
+		public HookMethodVisitor(final @Nullable MethodVisitor mv) {
+			super(Opcodes.ASM5, mv);
+			this.matcher = new MethodMatcher("net.minecraft.client.gui.FontRenderer", DescHelper.toDesc(void.class, String.class, int.class, int.class, int.class, int.class), "drawSplitString", "func_184177_bl");
+		}
+
+		/*@Override
+		public void visitCode() {
+			super.visitCode();
+			/*
+			 * 	 0  aload_0 [this]
+			 *   1  instanceof net.minecraft.tileentity.TileEntitySign [3]
+			 *   4  ifeq 11
+			 *   7  getstatic net.minecraft.tileentity.TileEntity.INFINITE_EXTENT_AABB : net.minecraft.util.math.AxisAlignedBB [16]
+			 *  10  areturn
+			 *  11  aload_0 [this]
+			 *
+			visitVarInsn(Opcodes.ALOAD, 0);
+			visitTypeInsn(Opcodes.INSTANCEOF, "net/minecraft/tileentity/TileEntitySign");
+			final Label skipReturn = new Label();
+			visitJumpInsn(Opcodes.IFEQ, skipReturn);
+			visitFieldInsn(Opcodes.GETSTATIC, "net/minecraft/tileentity/TileEntity", "INFINITE_EXTENT_AABB", DescHelper.toDesc("net.minecraft.util.math.AxisAlignedBB"));
+			visitInsn(Opcodes.ARETURN);
+			visitLabel(skipReturn);
+		}*/
+
+		@Override
+		public void visitMethodInsn(final int opcode, final @Nullable String owner, final @Nullable String name, final @Nullable String desc, final boolean itf) {
+			if (name!=null&&desc!=null&&this.matcher.match(name, desc))
+				super.visitMethodInsn(Opcodes.INVOKESTATIC, "com/kamesuta/mc/signpic/render/CustomBookRenderer", "hookDrawSplitString", desc, itf);
+			else
+				super.visitMethodInsn(opcode, owner, name, desc, itf);
+		}
+	}
+
+	private final MethodMatcher matcher;
+
+	public GuiScreenBookVisitor(final String obfClassName, final ClassVisitor cv) {
+		super(Opcodes.ASM5, cv);
+		this.matcher = new MethodMatcher(obfClassName, DescHelper.toDesc(void.class, new Object[0]), "drawScreen", "func_184177_bl");
+	}
+
+	@Override
+	public @Nullable MethodVisitor visitMethod(final int access, final @Nullable String name, final @Nullable String desc, final @Nullable String signature, final @Nullable String[] exceptions) {
+		final MethodVisitor parent = super.visitMethod(access, name, desc, signature, exceptions);
+		if (name==null||desc==null)
+			return parent;
+		return this.matcher.match(name, desc) ? new HookMethodVisitor(parent) : parent;
+	}
+}
--- src/main/java/com/kamesuta/mc/signpic/render/CustomBookRenderer.java
+++ src/main/java/com/kamesuta/mc/signpic/render/CustomBookRenderer.java
@@ -1,8 +1,13 @@
 package com.kamesuta.mc.signpic.render;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.annotation.Nullable;
 
 import com.kamesuta.mc.bnnwidget.render.WRenderer;
+
+import net.minecraft.client.gui.FontRenderer;
 
 public class CustomBookRenderer {
 
@@ -14,7 +19,18 @@ public class CustomBookRenderer {
 
 	}
 
-	public static void hookDrawSplitString(final @Nullable String p_78279_1_, final int p_78279_2_, final int p_78279_3_, final int p_78279_4_, final int p_78279_5_) {
-		WRenderer.font().drawSplitString(p_78279_1_, p_78279_2_, p_78279_3_, p_78279_4_, p_78279_5_);
+	public static void hookDrawSplitString(@Nullable String p_78279_1_, final int p_78279_2_, int p_78279_3_, final int p_78279_4_, final int p_78279_5_) {
+		final FontRenderer f = WRenderer.font();
+
+		while (p_78279_1_!=null&&p_78279_1_.endsWith("\n"))
+			p_78279_1_ = p_78279_1_.substring(0, p_78279_1_.length()-1);
+
+		final List<?> list = f.listFormattedStringToWidth(p_78279_1_, p_78279_4_);
+
+		for (final Iterator<?> iterator = list.iterator(); iterator.hasNext(); p_78279_3_ += f.FONT_HEIGHT) {
+			final String s1 = (String) iterator.next();
+			f.drawString(s1, p_78279_2_, p_78279_3_, p_78279_4_);
+		}
+		//f.drawSplitString(p_78279_1_, p_78279_2_, p_78279_3_, p_78279_4_, p_78279_5_);
 	}
 }
