f053f8dd1fa8d7afed2eaaacaa728d2a4de3397c
Kamesuta <kamesuta@gmail.com>
2017-02-19 20:01:17 +0900

    default invoke

--- src/main/java/com/kamesuta/mc/signpic/render/CustomBookRenderer.java
+++ src/main/java/com/kamesuta/mc/signpic/render/CustomBookRenderer.java
@@ -22,14 +22,6 @@ import net.minecraft.client.gui.FontRenderer;
 
 public class CustomBookRenderer {
 
-	public CustomBookRenderer() {
-		// TODO 自動生成されたコンストラクター・スタブ
-	}
-
-	public static void renderBook() {
-
-	}
-
 	public static void hookDrawSplitString(@Nonnull final String s, final int x, final int y, final int width, final int color) {
 		final FontRenderer f = WRenderer.font();
 
@@ -37,6 +29,7 @@ public class CustomBookRenderer {
 
 		final String[] lines = str.split("\n");
 		final List<String> newlines = Lists.newLinkedList();
+		boolean flag = false;
 		for (String line : lines) {
 			int num = 0;
 			final int index = StringUtils.lastIndexOf(line, "}");
@@ -44,6 +37,7 @@ public class CustomBookRenderer {
 				final String s2 = StringUtils.substring(line, 0, index+1);
 				final Entry entry = EntryId.from(s2).entry();
 				if (entry.isValid()) {
+					flag = true;
 					newlines.add(s2);
 					final List<?> list = f.listFormattedStringToWidth(line, width);
 					line = StringUtils.substringAfterLast(line, "}");
@@ -58,39 +52,40 @@ public class CustomBookRenderer {
 			for (int i = 0; i<num; i++)
 				newlines.add("");
 		}
+		if (flag) {
+			int iy = y;
+			for (final Iterator<String> itr = newlines.iterator(); itr.hasNext();) {
+				final String s1 = itr.next();
+				final int index = StringUtils.lastIndexOf(s1, "}");
+				c: {
+					if (index!=StringUtils.INDEX_NOT_FOUND) {
+						final String s2 = StringUtils.substring(s1, 0, index+1);
+						final Entry entry = EntryId.from(s2).entry();
+						if (entry.isValid()) {
+							final GuiImage gui = entry.getGui();
+							final CompoundAttr attr = entry.getMeta();
+							final Content content = entry.getContent();
+							final SizeData size00 = attr.sizes.getMovie().get();
+							final SizeData size01 = content!=null ? content.image.getSize() : SizeData.DefaultSize;
+							final SizeData size1 = size00.aspectSize(size01);
+							final SizeData size2 = ImageSizes.INNER.defineSize(size1, SizeData.create(f.FONT_HEIGHT, f.FONT_HEIGHT));
+							OpenGL.glPushMatrix();
+							OpenGL.glTranslatef(x, iy, 0f);
+							OpenGL.glScalef(size2.getWidth(), size2.getHeight(), 1f);
+							OpenGL.glTranslatef(size1.getWidth()/2, size1.getHeight()+(size1.getHeight()>=0 ? 0 : -size1.getHeight())-.5f, 0f);
+							OpenGL.glScalef(1f, -1f, 1f);
+							gui.renderSignPicture(1f, 4f);
 
-		int iy = y;
-		for (final Iterator<String> itr = newlines.iterator(); itr.hasNext();) {
-			final String s1 = itr.next();
-			final int index = StringUtils.lastIndexOf(s1, "}");
-			c: {
-				if (index!=StringUtils.INDEX_NOT_FOUND) {
-					final String s2 = StringUtils.substring(s1, 0, index+1);
-					final Entry entry = EntryId.from(s2).entry();
-					if (entry.isValid()) {
-						final GuiImage gui = entry.getGui();
-						final CompoundAttr attr = entry.getMeta();
-						final Content content = entry.getContent();
-						final SizeData size00 = attr.sizes.getMovie().get();
-						final SizeData size01 = content!=null ? content.image.getSize() : SizeData.DefaultSize;
-						final SizeData size1 = size00.aspectSize(size01);
-						final SizeData size2 = ImageSizes.INNER.defineSize(size1, SizeData.create(f.FONT_HEIGHT, f.FONT_HEIGHT));
-						OpenGL.glPushMatrix();
-						OpenGL.glTranslatef(x, iy, 0f);
-						OpenGL.glScalef(size2.getWidth(), size2.getHeight(), 1f);
-						OpenGL.glTranslatef(size1.getWidth()/2, size1.getHeight()+(size1.getHeight()>=0 ? 0 : -size1.getHeight())-.5f, 0f);
-						OpenGL.glScalef(1f, -1f, 1f);
-						gui.renderSignPicture(1f, 4f);
-
-						OpenGL.glPopMatrix();
-						WRenderer.startTexture();
-						break c;
+							OpenGL.glPopMatrix();
+							WRenderer.startTexture();
+							break c;
+						}
 					}
+					f.drawString(s1, x, iy, color);
+					iy += f.FONT_HEIGHT;
 				}
-				f.drawString(s1, x, iy, color);
-				iy += f.FONT_HEIGHT;
 			}
-		}
-		// f.drawSplitString(p_78279_1_, p_78279_2_, p_78279_3_, p_78279_4_, p_78279_5_);
+		} else
+			f.drawSplitString(s, x, y, width, color);
 	}
 }
