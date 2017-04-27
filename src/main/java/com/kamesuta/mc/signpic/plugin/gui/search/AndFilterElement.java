1b2623a23db252c8397a350781a0b4443ddae4c7
sjcl <sjcl@169.254.109.47>
2017-04-27 15:01:41 +0900

    AndFilterElement

---
+++ src/main/java/com/kamesuta/mc/signpic/plugin/gui/search/AndFilterElement.java
@@ -0,0 +1,22 @@
+package com.kamesuta.mc.signpic.plugin.gui.search;
+
+import javax.annotation.Nonnull;
+
+import com.kamesuta.mc.signpic.plugin.SignData;
+
+public class AndFilterElement implements IFilterElement {
+
+	public final @Nonnull IFilterElement filter1;
+	public final @Nonnull IFilterElement filter2;
+
+	public AndFilterElement(IFilterElement filter1, IFilterElement filter2) {
+		this.filter1 = filter1;
+		this.filter2 = filter2;
+	}
+
+	@Override
+	public boolean filter(SignData data) {
+		return this.filter1.filter(data)&&this.filter2.filter(data);
+	}
+
+}
