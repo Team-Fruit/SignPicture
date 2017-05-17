ca8afa6a09f156fe16be7324f8ce0c10522218f6
sjcl <sjcl@10.2.100.238>
2017-05-17 11:36:00 +0900

    OrFilter

--- src/main/java/com/kamesuta/mc/signpic/plugin/search/FilterParser.java
+++ src/main/java/com/kamesuta/mc/signpic/plugin/search/FilterParser.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -15,52 +16,53 @@ public class FilterParser {
 
 	public static @Nonnull SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-	public final @Nonnull String str;
-
-	public FilterParser(String str) {
-		this.str = str;
-	}
-
-	public IModCount<IFilterElement> parse() {
+	public static @Nonnull IModCount<IFilterElement> parse(String str) {
 		IModCount<IFilterElement> list = new NotifyCollections.NotifyArrayList<IFilterElement>();
 		String[] array = StringUtils.replaceChars(str, '\u3000', ' ').split(" ");
+		IFilterElement before = null;
 		for (int i = 0; i<array.length; i++) {
 			String line = array[i];
-			if (line.contains(":")) {
-				String[] pair = line.split(":");
-				if (pair.length>2) {
-					StringBuilder sb = new StringBuilder();
-					for (int p = 1; i<pair.length; p++)
-						sb.append(pair[p]);
-					pair = new String[] { pair[0], sb.toString() };
-				}
-				String key = pair[0];
-				String value = pair[1];
-				if (key.equalsIgnoreCase("since")||key.equalsIgnoreCase("until")) {
-					try {
-						Date date = dateFormat.parse(value);
-						if (key.equalsIgnoreCase("since"))
-							list.add(new DateFilterElement.AfterDateFilterElement(DateFilterProperty.CREATE, date));
-						else
-							list.add(new DateFilterElement.BeforeDateFilterElement(DateFilterProperty.CREATE, date));
-					} catch (ParseException e) { //NO-OP
-					}
-				} else {
-					StringFilterProperty property = StringFilterProperty.valueOf(key.toUpperCase());
-					if (property!=null)
-						list.add(new StringFilterElement.EqualsIgnoreCaseStringFilterElement(property, value));
-				}
-			} else if (line.startsWith("\"")&&line.endsWith("\"")) {
-				line = StringUtils.removeStart(StringUtils.removeEnd(line, "\""), "\"");
-				list.add(new UniversalFilterElement.EqualsIgnoreCaseUniversalFilterElement(line));
-			} else {
-				list.add(new UniversalFilterElement.ContainsIgnoreCaseUniversalFilterElement(line));
-			}
+			if (line.equals("OR")&&before!=null&&i+1<array.length)
+				before = new OrFilterElement(before, parseElement(array[++i]));
+			else
+				before = parseElement(line);
+			if (before!=null)
+				list.add(before);
 		}
 		return list;
 	}
 
-	public static IModCount<IFilterElement> parse(String str) {
-		return new FilterParser(str).parse();
+	private static @Nullable IFilterElement parseElement(String str) {
+		if (str.contains(":")) {
+			String[] pair = str.split(":");
+			if (pair.length>2) {
+				StringBuilder sb = new StringBuilder();
+				for (int p = 1; p<pair.length; p++)
+					sb.append(pair[p]);
+				pair = new String[] { pair[0], sb.toString() };
+			}
+			String key = pair[0];
+			String value = pair[1];
+			if (key.equalsIgnoreCase("since")||key.equalsIgnoreCase("until")) {
+				try {
+					Date date = dateFormat.parse(value);
+					if (key.equalsIgnoreCase("since"))
+						return new DateFilterElement.AfterDateFilterElement(DateFilterProperty.CREATE, date);
+					else
+						return new DateFilterElement.BeforeDateFilterElement(DateFilterProperty.CREATE, date);
+				} catch (ParseException e) { //NO-OP
+				}
+			} else {
+				StringFilterProperty property = StringFilterProperty.valueOf(key.toUpperCase());
+				if (property!=null)
+					return new StringFilterElement.EqualsIgnoreCaseStringFilterElement(property, value);
+			}
+		} else if (str.startsWith("\"")&&str.endsWith("\"")) {
+			str = StringUtils.removeStart(StringUtils.removeEnd(str, "\""), "\"");
+			return new UniversalFilterElement.EqualsIgnoreCaseUniversalFilterElement(str);
+		} else
+			return new UniversalFilterElement.ContainsIgnoreCaseUniversalFilterElement(str);
+		return null;
 	}
+
 }
--- src/main/java/com/kamesuta/mc/signpic/plugin/search/OrFilterElement.java
+++ src/main/java/com/kamesuta/mc/signpic/plugin/search/OrFilterElement.java
@@ -4,20 +4,19 @@ import javax.annotation.Nonnull;
 
 import com.kamesuta.mc.signpic.plugin.SignData;
 
-@Deprecated
-public class AndFilterElement implements IFilterElement {
+public class OrFilterElement implements IFilterElement {
 
 	public final @Nonnull IFilterElement filter1;
 	public final @Nonnull IFilterElement filter2;
 
-	public AndFilterElement(IFilterElement filter1, IFilterElement filter2) {
+	public OrFilterElement(IFilterElement filter1, IFilterElement filter2) {
 		this.filter1 = filter1;
 		this.filter2 = filter2;
 	}
 
 	@Override
 	public boolean filter(SignData data) {
-		return this.filter1.filter(data)&&this.filter2.filter(data);
+		return this.filter1.filter(data)||this.filter2.filter(data);
 	}
 
 }
