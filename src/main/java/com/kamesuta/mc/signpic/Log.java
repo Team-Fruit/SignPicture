28225db0affd92e1fa1a9637053e0394b9a3b910
sjcl <fruit@bebehp.com>
2017-06-11 13:12:57 +0900

    Log

--- src/main/java/com/kamesuta/mc/signpic/Log.java
+++ src/main/java/com/kamesuta/mc/signpic/Log.java
@@ -18,7 +18,7 @@ public class Log {
 	public static @Nonnull Logger dev = new DevLogger(log);
 
 	private static class DevLogger extends AbstractLogger {
-		private Logger logger;
+		private final Logger logger;
 
 		public DevLogger(@Nonnull final Logger logger) {
 			this.logger = logger;
@@ -31,35 +31,101 @@ public class Log {
 		}
 
 		@Override
-		protected boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable Message data, final @Nullable Throwable t) {
+		public boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable Message data, final @Nullable Throwable t) {
 			return isEnabled();
 		}
 
 		@Override
-		protected boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable Object data, final @Nullable Throwable t) {
+		public boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable Object data, final @Nullable Throwable t) {
 			return isEnabled();
 		}
 
 		@Override
-		protected boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable String data) {
+		public boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable String data) {
 			return isEnabled();
 		}
 
 		@Override
-		protected boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable String data, final @Nullable Object... p1) {
+		public boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable String data, final @Nullable Object... p1) {
 			return isEnabled();
 		}
 
 		@Override
-		protected boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable String data, final @Nullable Throwable t) {
+		public boolean isEnabled(final @Nullable Level level, final @Nullable Marker marker, final @Nullable String data, final @Nullable Throwable t) {
 			return isEnabled();
 		}
 
 		@Override
-		public void log(final @Nullable Marker marker, final @Nullable String fqcn, final @Nullable Level level, final @Nullable Message data, final @Nullable Throwable t) {
+		public void log(final @Nullable Level level, final @Nullable Marker marker, final @Nullable Message data, final @Nullable Throwable t) {
 			if (isEnabled()&&data!=null)
 				this.logger.log(level, marker, "[DEBUG] "+data.getFormattedMessage(), t);
 		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final CharSequence message, @Nullable final Throwable t) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1, @Nullable final Object p2) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1, @Nullable final Object p2, @Nullable final Object p3) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1, @Nullable final Object p2, @Nullable final Object p3, @Nullable final Object p4) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1, @Nullable final Object p2, @Nullable final Object p3, @Nullable final Object p4, @Nullable final Object p5) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1, @Nullable final Object p2, @Nullable final Object p3, @Nullable final Object p4, @Nullable final Object p5, @Nullable final Object p6) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1, @Nullable final Object p2, @Nullable final Object p3, @Nullable final Object p4, @Nullable final Object p5, @Nullable final Object p6, @Nullable final Object p7) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1, @Nullable final Object p2, @Nullable final Object p3, @Nullable final Object p4, @Nullable final Object p5, @Nullable final Object p6, @Nullable final Object p7, @Nullable final Object p8) {
+			return isEnabled();
+		}
+
+		@Override
+		public boolean isEnabled(@Nullable final Level level, @Nullable final Marker marker, @Nullable final String message, @Nullable final Object p0, @Nullable final Object p1, @Nullable final Object p2, @Nullable final Object p3, @Nullable final Object p4, @Nullable final Object p5, @Nullable final Object p6, @Nullable final Object p7, @Nullable final Object p8, @Nullable final Object p9) {
+			return isEnabled();
+		}
+
+		@Override
+		public void logMessage(final @Nullable String fqcn, final @Nullable Level level, final @Nullable Marker marker, final @Nullable Message message, final @Nullable Throwable t) {
+			logMessage(fqcn, level, marker, message, t);
+		}
+
+		@Override
+		public Level getLevel() {
+			return Level.DEBUG;
+		}
+
 	}
 
 	@SuppressWarnings("deprecation")
