dba887791b3e8b3f76792823b93bcd29212e07fc
sjcl <fruit@bebehp.com>
2017-06-12 16:23:23 +0900

    #

--- src/main/java/com/kamesuta/mc/signpic/util/ChatBuilder.java
+++ src/main/java/com/kamesuta/mc/signpic/util/ChatBuilder.java
@@ -151,7 +151,7 @@ public class ChatBuilder {
 			if (chat.useId)
 				mc.ingameGUI.getChatGUI().printChatMessageWithOptionalDeletion(msg, chat.id);
 			else
-				mc.thePlayer.addChatComponentMessage(msg);
+				mc.thePlayer.addChatComponentMessage(msg, false);
 		}
 	}
 
