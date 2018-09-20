62265ce1d3b070201e8a8480f46fd76d37920e67
Kamesuta <kamesuta@gmail.com>
2018-09-21 00:40:29 +0900

    remove syntax error

--- 1.7.10/src/main/java/com/kamesuta/mc/signpic/SignPicture.java
+++ 1.7.10/src/main/java/com/kamesuta/mc/signpic/SignPicture.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.kamesuta.mc.signpic.compat.Compat.CompatProxy;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -21,7 +23,7 @@ public class SignPicture {
 	public static @Nullable SignPicture instance;
 
 	@SidedProxy(serverSide = Reference.PROXY_SERVER, clientSide = Reference.PROXY_CLIENT)
-	public static @Nullable CommonProxy proxy;
+	public static @Nullable CompatProxy proxy;
 
 	@NetworkCheckHandler
 	public boolean checkModList(final @Nonnull Map<String, String> versions, final @Nonnull Side side) {
