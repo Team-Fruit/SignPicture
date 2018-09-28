96d44b621dec872f5e9a024a49e011fc0dca6403
Kamesuta <kamesuta@gmail.com>
2018-09-28 22:31:19 +0900

    fix build error

---
+++ common/src/main/java/com/kamesuta/mc/signpic/SignPicture.java
@@ -0,0 +1,50 @@
+package com.kamesuta.mc.signpic;
+
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.kamesuta.mc.signpic.compat.Compat.CompatProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkCheckHandler;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY)
+public class SignPicture {
+	@Instance(Reference.MODID)
+	public static @Nullable SignPicture instance;
+
+	@SidedProxy(serverSide = Reference.PROXY_SERVER, clientSide = Reference.PROXY_CLIENT)
+	public static @Nullable CompatProxy proxy;
+
+	@NetworkCheckHandler
+	public boolean checkModList(final @Nonnull Map<String, String> versions, final @Nonnull Side side) {
+		return true;
+	}
+
+	@EventHandler
+	public void preInit(final @Nonnull FMLPreInitializationEvent event) {
+		if (proxy!=null)
+			proxy.preInit(event);
+	}
+
+	@EventHandler
+	public void init(final @Nonnull FMLInitializationEvent event) {
+		if (proxy!=null)
+			proxy.init(event);
+	}
+
+	@EventHandler
+	public void postInit(final @Nonnull FMLPostInitializationEvent event) {
+		if (proxy!=null)
+			proxy.postInit(event);
+	}
+}
