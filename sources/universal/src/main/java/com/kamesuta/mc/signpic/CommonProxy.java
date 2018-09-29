c4e02a55e2515a363d800cdca110d1a29f7c994a
Kamesuta <kamesuta@gmail.com>
2018-09-29 23:01:53 +0900

    new archetype

---
+++ sources/universal/src/main/java/com/kamesuta/mc/signpic/CommonProxy.java
@@ -0,0 +1,19 @@
+package com.kamesuta.mc.signpic;
+
+import javax.annotation.Nonnull;
+
+import com.kamesuta.mc.signpic.compat.CompatProxy;
+
+public class CommonProxy implements CompatProxy {
+	@Override
+	public void preInit(final @Nonnull CompatFMLPreInitializationEvent event) {
+	}
+
+	@Override
+	public void init(final @Nonnull CompatFMLInitializationEvent event) {
+	}
+
+	@Override
+	public void postInit(final @Nonnull CompatFMLPostInitializationEvent event) {
+	}
+}
