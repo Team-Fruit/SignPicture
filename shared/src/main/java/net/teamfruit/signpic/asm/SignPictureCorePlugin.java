a791417e74bd7132d3b8b7be9b673f66fd77b26f
Kamesuta <kamesuta@gmail.com>
2020-06-06 02:23:30 +0900

    move

---
+++ shared/src/main/java/net/teamfruit/signpic/asm/SignPictureCorePlugin.java
@@ -0,0 +1,36 @@
+package net.teamfruit.signpic.asm;
+
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import net.teamfruit.signpic.Reference;
+
+public class SignPictureCorePlugin implements IFMLLoadingPlugin {
+	@Override
+	public @Nullable String[] getASMTransformerClass() {
+		return new String[] {
+				Reference.TRANSFORMER
+		};
+	}
+
+	@Override
+	public @Nullable String getModContainerClass() {
+		return null;
+	}
+
+	@Override
+	public @Nullable String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(final @Nullable Map<String, Object> data) {
+	}
+
+	@Override
+	public @Nullable String getAccessTransformerClass() {
+		return null;
+	}
+}
